93195ac492ebf70ec44676fc9b23ccbb30308906
Mesiow <wysockichris75@yahoo.com>
2025-01-31 22:43:38 -0500

    add loading indicator helper method

--- Restaurant Viewer Project/Extensions/UIViewController+Ext.swift
+++ Restaurant Viewer Project/Extensions/UIViewController+Ext.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 
+fileprivate var aView: UIView?
+
 extension UIViewController {
     func presentAlertOnMainThread(title: String, message: String) {
         DispatchQueue.main.async {
             let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
             self.present(alert, animated: true)
+        }
+    }
+    
+    func showActivityLoadingView(){
+        aView = UIView(frame: self.view.bounds)
+        aView!.backgroundColor = .clear
+        
+        let activityView = UIActivityIndicatorView(style: .large)
+        activityView.color = .black
+        activityView.center = aView!.center
+        activityView.startAnimating()
+        aView!.addSubview(activityView)
+        self.view.addSubview(aView!)
+    }
+    
+    func removeActivityLoadingView(){
+        if aView != nil {
+            aView!.removeFromSuperview()
+            aView = nil
         }
     }
 }
