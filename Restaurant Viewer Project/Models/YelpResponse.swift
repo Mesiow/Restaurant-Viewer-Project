ce5bd52e41f4d68572a45043366fcf339b758a4d
Mesiow <wysockichris75@yahoo.com>
2025-01-31 22:35:34 -0500

    our json response model from our network call

--- Restaurant Viewer Project/Models/YelpResponse.swift
+++ Restaurant Viewer Project/Models/YelpResponse.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct Business: Codable {
+    let name: String
+    let rating: Double
+    let price: String?
+    let location: Location
+    let image_url: String
+}
+
+struct Location: Codable {
+    let address1: String
+    let city: String
+    let state: String
+    let zip_code: String
+}
+
+struct YelpResponse: Codable {
+    let businesses: [Business]
+}
