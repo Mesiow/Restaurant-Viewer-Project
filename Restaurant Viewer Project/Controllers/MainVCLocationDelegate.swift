e618a425dfdafcbde25c38dd9b9c7e661f601081
Mesiow <wysockichris75@yahoo.com>
2025-01-31 22:34:17 -0500

    location delegate functions for our MainVC class

--- Restaurant Viewer Project/Controllers/MainVCLocationDelegate.swift
+++ Restaurant Viewer Project/Controllers/MainVCLocationDelegate.swift
@@ -5,4 +5,38 @@
 //  Created by Chris W on 1/31/25.
 //
 
-import Foundation
+import UIKit
+import CoreLocation
+
+extension MainVC: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            print("in use")
+            manager.startUpdatingLocation()
+            
+        case .denied, .restricted:
+            self.presentAlertOnMainThread(title: "Location Permission Denied", message: "Enable location services in settings")
+            
+        case .notDetermined:
+            print("not determined")
+            manager.requestWhenInUseAuthorization()
+            
+        @unknown default:
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            //stop updating once we get the first location
+            locationManager.stopUpdatingLocation()
+            currentLocation = location
+            
+            let lat = location.coordinate.latitude
+            let long = location.coordinate.longitude
+            fetchRestaurantDataAtLocation(latitude: lat, longitude: long)
+        }
+    }
+}
